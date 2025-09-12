652653ec6e82e7ae937b529572f0c18f4e3a1e0d
irina-vereshchagina <68503326+irina-vereshchagina@users.noreply.github.com>
2025-09-13 00:33:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart
 from aiogram.fsm.storage.memory import MemoryStorage
+
 from config import TELEGRAM_BOT_TOKEN
 from handlers import start, info, prompt, generation, vectorize
 from utils.user_state import get_user_state, STATE_GENERATE, STATE_VECTORIZE, STATE_MENU
-from utils.user_roles import load_db
+
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("aiogram.event").setLevel(logging.DEBUG)
@@ -17,11 +18,14 @@ defaults = DefaultBotProperties(parse_mode=ParseMode.HTML)
 bot = Bot(token=TELEGRAM_BOT_TOKEN, default=defaults)
 dp = Dispatcher(storage=MemoryStorage())
 
+
 def is_generate_text(message):
     return (
-        message.text and not message.text.startswith("/")
+        message.text
+        and not message.text.startswith("/")
         and get_user_state(message.from_user.id) == STATE_GENERATE
     )
+
 
 def is_vectorization_photo(message):
     return (
@@ -29,15 +33,16 @@ def is_vectorization_photo(message):
         and get_user_state(message.from_user.id) == STATE_VECTORIZE
     )
 
+
 # Регистрация хендлеров
 dp.message.register(start.start, CommandStart())
-dp.message.register(start.setrole_command, Command(commands=["setrole"]))
 dp.message.register(start.start, lambda m: m.text == "⬅️ В меню")
 dp.message.register(info.info, lambda m: m.text == "ℹ️ Информация")
 dp.message.register(prompt.prompt_for_idea, lambda m: m.text == "🎨 Генерация логотипа")
 dp.message.register(vectorize.ask_for_image, lambda m: m.text == "🖼 Векторизация")
 dp.message.register(vectorize.handle_vectorization_image, is_vectorization_photo)
 dp.message.register(generation.handle_idea, is_generate_text)
+
 
 @dp.message()
 async def fallback_handler(message):
@@ -51,6 +56,6 @@ async def fallback_handler(message):
     else:
         await message.answer("❓ Непонятное состояние. Нажмите '⬅️ В меню'.")
 
+
 if __name__ == "__main__":
-    load_db()
     asyncio.run(dp.start_polling(bot))
